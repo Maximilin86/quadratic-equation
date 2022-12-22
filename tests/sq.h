ec1425e2c0df51243e198e602c36292e31fa02d2
Maximilin86 <max_ilin86@mail.ru>
2022-12-22 15:25:01 +0300

    test2

--- tests/sq.h
+++ tests/sq.h
@@ -12,3 +12,18 @@ TEST(solutionSquareEquation, test1)
     EXPECT_EQ(result[0], -1);
     /*ASSERT_THAT(0, Eq(0)); */
 }
+TEST(solutionSquareEquation, test2)
+{
+    double *result = solutionSquareEquation(2,5,2);
+    //double a[2] = {-3,1};
+    EXPECT_EQ(result[0], -3);
+    EXPECT_EQ(result[1], 1);
+    /*ASSERT_THAT(0, Eq(0)); */
+}
+TEST(solutionSquareEquation, test3)
+{
+    double *result = solutionSquareEquation(1,1,1);
+    //double a[2] = {-3,1};
+    EXPECT_EQ(result, nullptr);
+    /*ASSERT_THAT(0, Eq(0)); */
+}
