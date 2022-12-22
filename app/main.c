342c4defeb401db713707a227411d35b277cf887
Maximilin86 <max_ilin86@mail.ru>
2022-12-22 16:43:45 +0300

    test4

--- app/main.c
+++ app/main.c
@@ -4,11 +4,11 @@
 
 int main()
 {
-    int a, b, c;
-    printf("Enter variables A B C \n");
-    scanf("%d %d %d", &a, &b, &c);
+   // int a, b, c;
+   // printf("Enter variables A B C \n");
+   // scanf("%d %d %d", &a, &b, &c);
 
-    double *result = solutionSquareEquation(a,b,c);
+    double *result = solutionSquareEquation(2,5,2);
     if(result==NULL) printf("There are no roots.\n");
         else   if(result[0]== result[1]) printf("One root: %f\n", result[0]);
                     else  printf("Roots: %f %f\n", result[0], result[1]);
